f5fea190a2f9d2a0ce84555d8b8c4e79774e6b98
Furkid <nil901121@gmail.com>
2024-07-29 20:42:45 +0800

    Refactor GithubRepository to include caching and error handling
    - Added caching with GithubCache to store and retrieve search results.
    - Enhanced error handling to catch and rethrow SearchResultError from the API client.
    - Updated constructor to allow optional GithubApiClient dependency injection.

--- packages/github_search/github_repository/lib/github_repository.dart
+++ packages/github_search/github_repository/lib/github_repository.dart
@@ -2,3 +2,4 @@ library github_repository;
 
 export 'src/models/models.dart';
 export 'src/github_repository.dart';
+export 'src/github_cache.dart';
--- packages/github_search/github_repository/lib/src/github_repository.dart
+++ packages/github_search/github_repository/lib/src/github_repository.dart
@@ -1,24 +1,46 @@
-import 'package:github_api_client/github_api_client.dart' show GithubApiClient, SearchResult;
+import 'package:github_api_client/github_api_client.dart'
+    show GithubApiClient, SearchResult, SearchResultError;
+import 'github_cache.dart';
 import 'models/models.dart';
 
 class GithubRepository {
-  final GithubApiClient client;
+  final GithubApiClient _client;
+  final GithubCache cache;
 
   GithubRepository({
-    required this.client,
-  });
+    GithubApiClient? client,
+    required this.cache,
+  }) : _client = client ?? GithubApiClient();
 
   Future<RepositorySearchResult> githubRepositorySearch(String term) async {
-    final SearchResult result = await client.githubRepositorySearch(term);
+    final RepositorySearchResult? cacheResult = cache.get(term);
+    if (cacheResult != null) {
+      final List<RepositoryResultItem> items = cacheResult.items
+          .map((item) => RepositoryResultItem(
+                fullName: item.fullName,
+                avatarUrl: item.avatarUrl,
+                htmlUrl: item.htmlUrl,
+              ))
+          .toList();
+      return RepositorySearchResult(items: items);
+    }
 
-    final List<RepositoryResultItem> items = result.items.map((item) =>
-        RepositoryResultItem(
-          fullName: item.fullName,
-          avatarUrl: item.owner.avatarUrl,
-          htmlUrl: item.htmlUrl,
-        )
-    ).toList();
-
-    return RepositorySearchResult(items: items);
+    try {
+      final SearchResult result = await _client.githubRepositorySearch(term);
+      final List<RepositoryResultItem> items = result.items
+          .map((item) => RepositoryResultItem(
+                fullName: item.fullName,
+                avatarUrl: item.owner.avatarUrl,
+                htmlUrl: item.htmlUrl,
+              ))
+          .toList();
+      cache.set(term, RepositorySearchResult(items: items));
+      return RepositorySearchResult(items: items);
+    } catch (error) {
+      error is SearchResultError
+          ? throw RepositorySearchError.fromJson(error.toJson())
+          : throw const RepositorySearchError(
+              message: 'An unknown error occurred');
+    }
   }
 }
